e39b97ae3296939a441976e5e1d648dfb62bc103
Lois <loisisar@outlook.com>
2023-11-28 03:12:11 +0000

    create text element if children is an object

--- app.js
+++ app.js
@@ -17,3 +17,23 @@ text["nodeValue"] = element.props.children;
 
 node.appendChild(text);
 container.appendChild(node)
+
+function createElement(type, props, ...children) {
+    return {
+        type,
+        props: {
+            ...props,
+            children: children.map(child => typeof child === "object" ? child : createTextElement(child)),
+        }
+    }
+}
+
+function createTextElement(text) {
+    return {
+        type: "TEXT_ELEMENT",
+        props: {
+            nodeValue: text,
+            children: [],
+        }
+    }
+}
