6010a692d81658053e15431be766756afccd2d97
Lois <loisisar@outlook.com>
2023-11-28 06:09:18 +0000

    before creating fiber

--- app.js
+++ app.js
@@ -12,6 +12,23 @@ function render(element, container) {
     container.appendChild(dom);
 }
 
+let nextUnitOfWork = null;
+
+function workLoop(deadline) {
+    let shouldYield = false;
+    while (nextUnitOfWork && !shouldYield) {
+        nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
+
+        shouldYield = deadline.timeRemaining() < 1;
+    }
+
+    requestIdleCallback(workLoop);
+}
+
+function performUnitOfWork(nextUnitOfWork) {
+    // TODO
+}
+
 const Didact = {
     createElement,
     render,
